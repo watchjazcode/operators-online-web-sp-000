7bec530c066c61ed496dcf1fd0c6920335efe969
Jazmin M <jazminmerckel@gmail.com>
2019-09-22 21:53:43 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  else #conditionals after else are skipped over 
+    return false 
+  end
 end
 
 
 
-def not_safe?(speed)
-	
+def not_safe?(speed) 
+	#(speed > 60 || speed < 40) ? true : false
+	speed > 60 ? true : (speed) < 40
 end
-	
+#adding parenthesis around expression helps with order of operations
 
 
