6b94309a469afa36abdae10e86dc32589e7eb659
Jazmin M <jazminmerckel@gmail.com>
2019-09-22 21:51:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,7 +11,8 @@ end
 
 
 def not_safe?(speed) 
-	(speed > 60 || speed < 40) ? true : false
+	#(speed > 60 || speed < 40) ? true : false
+	speed > 60 ? true : (speed < 40)
 end
 #adding parenthesis around expression helps with order of operations
 
